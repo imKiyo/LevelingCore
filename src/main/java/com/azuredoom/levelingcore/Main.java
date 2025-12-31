e85b9b23652d8185c26af0cb7acf66429567f762
AzureDoom <7415711+AzureDoom@users.noreply.github.com>
2025-12-31 03:57:01 -0500

    Keep main test in until Hytale server api is ready to move to

--- src/main/java/com/azuredoom/levelingcore/Main.java
+++ src/main/java/com/azuredoom/levelingcore/Main.java
@@ -1,7 +1,14 @@
 package com.azuredoom.levelingcore;
 
+import com.azuredoom.levelingcore.config.ConfigManager;
+import com.azuredoom.levelingcore.config.LevelFormulaFactory;
+import com.azuredoom.levelingcore.database.DataSourceFactory;
+import com.azuredoom.levelingcore.database.JdbcLevelRepository;
+import com.azuredoom.levelingcore.level.LevelServiceImpl;
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.UUID;
 
 public class Main {
 
@@ -11,28 +18,28 @@ public class Main {
 
     static void main() {
         // TODO: Move to server startup so registration are auto handled
-        // var config = ConfigManager.loadOrCreate(configPath);
-        // var desc = LevelFormulaFactory.descriptorFromConfig(config);
-        // var formula = LevelFormulaFactory.fromConfig(config);
-        // var jdbcUrl = config.database.jdbcUrl;
-        // var user = config.database.username;
-        // var pass = config.database.password;
-        // var maxPoolSize = config.database.maxPoolSize;
-        // var dataSource = DataSourceFactory.create(jdbcUrl, user, pass, maxPoolSize);
-        // var repository = new JdbcLevelRepository(dataSource);
-        // if (config.formula.migrateXP) {
-        // repository.migrateFormulaIfNeeded(formula, desc);
-        // }
-        // var levelService = new LevelServiceImpl(formula, repository);
+        var config = ConfigManager.loadOrCreate(configPath);
+        var desc = LevelFormulaFactory.descriptorFromConfig(config);
+        var formula = LevelFormulaFactory.fromConfig(config);
+        var jdbcUrl = config.database.jdbcUrl;
+        var user = config.database.username;
+        var pass = config.database.password;
+        var maxPoolSize = config.database.maxPoolSize;
+        var dataSource = DataSourceFactory.create(jdbcUrl, user, pass, maxPoolSize);
+        var repository = new JdbcLevelRepository(dataSource);
+        if (config.formula.migrateXP) {
+            repository.migrateFormulaIfNeeded(formula, desc);
+        }
+        var levelService = new LevelServiceImpl(formula, repository);
 
         // TODO: Remove once hooks into the player/mob kill events are found and integrable.
-        // var testId = UUID.fromString("d3804858-4bb8-4026-ae21-386255ed467d");
-        // levelService.addXp(testId, 500);
+        var testId = UUID.fromString("d3804858-4bb8-4026-ae21-386255ed467d");
+        levelService.addXp(testId, 500);
         // TODO: Move to chat based logging instead of System loggers
-        // LOGGER.log(System.Logger.Level.INFO, String.format("XP: %d", levelService.getXp(testId)));
-        // LOGGER.log(System.Logger.Level.INFO, String.format("Level: %d", levelService.getLevel(testId)));
+        LOGGER.log(System.Logger.Level.INFO, String.format("XP: %d", levelService.getXp(testId)));
+        LOGGER.log(System.Logger.Level.INFO, String.format("Level: %d", levelService.getLevel(testId)));
 
         // TODO: Move to server shutdown so JDBC resources are properly closed
-        // Runtime.getRuntime().addShutdownHook(new Thread(repository::close));
+        Runtime.getRuntime().addShutdownHook(new Thread(repository::close));
     }
 }
